9e64ffe282b7af724773fc24619871eaca294665
Wim Bonis <wb@stylite.de>
2020-12-09 14:32:43 +0100

    Add more Things to custom.sh
    Set correct hostname

--- packer_templates/ubuntu/scripts/custom.sh
+++ packer_templates/ubuntu/scripts/custom.sh
@@ -2,11 +2,26 @@
 
 # Custom things i want in my template
 
+. /etc/lsb-release
+
+newhostname=$(echo "ubuntu-${DISTRIB_RELEASE}-template-sty"|tr "." "-")
+echo "Setting Hostname to $newhostname"
+hostnamectl set-hostname $newhostname
+
+echo "Setting German Keyboard on console"
+localectl set-keymap de nodeadkeys
+
 echo "remove nfs-common rpcbind"
 apt-get -y purge nfs-common rpcbind || true;
 
 echo "remove vagrant dev-packages"
 apt-get -y purge libssl-dev libreadline-dev zlib1g-dev linux-source dkms nfs-common linux-headers-$(uname -r) perl cifs-utils rsync 
+
+echo "remove snapd"
+apt-get -y purge snapd
+
+echo "remove geoip-database"
+apt-get -y purge geoip-database
 
 echo "autoremoving packages and cleaning apt data"
 apt-get -y autoremove;
