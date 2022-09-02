d96a79b09cecbd91d94f9cbe53c9ed6e6bc402bc
Wim Bonis <wb@stylite.de>
2022-09-02 17:05:30 +0200

    Add ubuntu 22.04 to script

--- build-vsphere-stynux.sh
+++ build-vsphere-stynux.sh
@@ -2,6 +2,7 @@
 
 cd packer_templates/ubuntu
 
+packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-22.04-amd64-vsphere.json
 packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-20.04-amd64-vsphere.json
 packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-18.04-amd64-vsphere.json
 packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-16.04-amd64-vsphere.json
