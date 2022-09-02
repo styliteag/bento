7ef058e289dcd8e06b7e5e4e5b189c0132717990
Wim Bonis <wb@stylite.de>
2022-09-02 15:23:49 +0200

    Fix vpshere -> vsphere

--- build-vsphere-stynux.sh
+++ build-vsphere-stynux.sh
@@ -2,6 +2,6 @@
 
 cd packer_templates/ubuntu
 
-packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-20.04-amd64-vpshere.json
-packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-18.04-amd64-vpshere.json
-packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-16.04-amd64-vpshere.json
+packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-20.04-amd64-vsphere.json
+packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-18.04-amd64-vsphere.json
+packer build -var-file=../../../stynux-vmware-cluster-10.30.4.10.json  -force ubuntu-16.04-amd64-vsphere.json
--- build-vsphere.sh
+++ build-vsphere.sh
@@ -2,6 +2,6 @@
 
 cd packer_templates/ubuntu
 
-packer build -var-file=../../../stylite-vmware-cluster-10.40.4.10.json  -force ubuntu-20.04-amd64-vpshere.json
-packer build -var-file=../../../stylite-vmware-cluster-10.40.4.10.json  -force ubuntu-18.04-amd64-vpshere.json
-packer build -var-file=../../../stylite-vmware-cluster-10.40.4.10.json  -force ubuntu-16.04-amd64-vpshere.json
+packer build -var-file=../../../stylite-vmware-cluster-10.40.4.10.json  -force ubuntu-20.04-amd64-vsphere.json
+packer build -var-file=../../../stylite-vmware-cluster-10.40.4.10.json  -force ubuntu-18.04-amd64-vsphere.json
+packer build -var-file=../../../stylite-vmware-cluster-10.40.4.10.json  -force ubuntu-16.04-amd64-vsphere.json
